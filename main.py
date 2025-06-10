441adba2c37e691f6e5456af0ee0527cf1c3a462
Shlomivaknin93 <shlomies150@gmail.com>
2025-06-10 18:58:22 +0300

    הוספתי תיקון לקובץ Flask

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
 
 @app.route('/playlist')
 def playlist():
     return render_template('playlist.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- static/js/script.js
+++ static/js/script.js
@@ -15,3 +15,40 @@ toggleButton.addEventListener('click', function() {
     isVideoActive = !isVideoActive; // משנה את המצב
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const audioPlayer = document.getElementById('audio-player');
+    const playBtn = document.getElementById('play-btn');
+    const pauseBtn = document.getElementById('pause-btn');
+    const video = document.getElementById('background-video');
+    const gif = document.getElementById('background-gif');
+    const switchBtn = document.getElementById('toggle-background');
+    const channelEffect = document.getElementById('channel-switch-effect');
+
+    let showVideo = true;
+
+    function switchBackground() {
+        // הצג את אפקט ההפרעה
+        channelEffect.style.opacity = '1';
+
+        // אחרי 300 מילישניות - החלף רקע והסתר את האפקט
+        setTimeout(() => {
+            showVideo = !showVideo;
+            video.style.display = showVideo ? 'block' : 'none';
+            gif.style.display = showVideo ? 'none' : 'block';
+
+            channelEffect.style.opacity = '0';
+        }, 300);
+    }
+
+    switchBtn.addEventListener('click', switchBackground);
+
+    audioPlayer.play().catch(function (error) {
+        console.log('Autoplay blocked:', error);
+    });
+
+    playBtn.addEventListener('click', () => audioPlayer.play());
+    pauseBtn.addEventListener('click', () => audioPlayer.pause());
+});
+
+
